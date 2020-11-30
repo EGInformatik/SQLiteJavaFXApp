2f22fd0020c240b270afe2f17c1366a81266d8d9
uldahlalex <alex@uldahl.dk>
2020-11-30 10:23:00 +0100

    read and add

--- src/sqlitejavafxapp/FXMLDocumentController.java
+++ src/sqlitejavafxapp/FXMLDocumentController.java
@@ -12,9 +12,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -28,60 +28,87 @@ public class FXMLDocumentController implements Initializable {
     
 
     @FXML
-    private Color x2;
-    @FXML
-    private Font x1;
-    @FXML
-    private Color x4;
-    @FXML
-    private Font x3;
-    @FXML
     private ListView<HashMap> passengerList;
     @FXML
     private TextArea currentPassenger;
+    @FXML
+    private TextField nameInput;
+    @FXML
+    private TextField ticketInput;
+    @FXML
+    private TextField fareInput;
 
+    DatabaseLayer db;
+    @FXML
+    private TextField ageInput;
+    @FXML
+    private TextField genderInput;
+    @FXML
+    private TextField survivedInput;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        DatabaseLayer dbl = new DatabaseLayer();
-        this.passengerList.setItems(dbl.readPassengers());
+        db = new DatabaseLayer();
+        this.passengerList.setItems(db.readPassengers());
         
     }    
 
     @FXML
     private void clickAddPassenger(ActionEvent event) {
+        db.addPassenger(nameInput.getText(), ticketInput.getText(), fareInput.getText());
     }
 
     @FXML
     private void clickDeletePassenger(ActionEvent event) {
+        db.deletePassenger(this.passengerList.getSelectionModel().getSelectedItem());
     }
+    
 
     @FXML
     private void clickPassenger(MouseEvent event) {
         HashMap person = this.passengerList.getSelectionModel().getSelectedItem();
         
+        String pronoun = this.figureOutPronoun(person);
+        String survived = this.figureOutSurvived(person);
+        double fare = Double.parseDouble((String)person.get("Fare"));
+        double fareAfterInflation = this.calculateFareAfterInflation(fare);
+        
         String text = person.get("Name")+"\nWas "+person.get("Age")+" years old at the time of being "
                         + "a passenger on Titanic.\n\n";
-        String pronoun = "";
-        if(person.get("Sex").equals("female")) {
-            pronoun = "She";
-        } else if(person.get("Sex").equals("male")) {
-            pronoun = "He";
-        }
-        double fare = Double.parseDouble((String)person.get("Fare"));
-        text = text+pronoun+" paid "+person.get("Fare")+" british pounds to embark on Titanic at the time.\n"
-                + "That equates to a total of "+this.calculateTodayPrice(fare)+" british pounds in 2020\n\n";
+       
+        text = text+pronoun+" paid "+fare+" british pounds to embark on Titanic at the time.\n"
+                + "That equates to a total of "+fareAfterInflation+" british pounds in 2020\n\n";
         
-        if(person.get("Survived").equals("0")) {
-            text = text+pronoun+" did not survive.";
-        } else if (person.get("Survived").equals("1")) {
-            text = text+pronoun+" managed to survive.";
-        }
+        text = text + pronoun + survived;
         this.currentPassenger.setText(text);
     }
     
-    public double calculateTodayPrice(double fare) {
+    public double calculateFareAfterInflation(double fare) {
         return fare*119;
+    }
+    
+    public String figureOutPronoun(HashMap person) {
+        if(person.get("Sex").equals("female")) {
+            return "She";
+        } else if(person.get("Sex").equals("male")) {
+            return "He";
+        }
+        return "They";
+    }
+    
+    public String figureOutSurvived(HashMap person) {
+        if(person.get("Survived").equals("0")) {
+            return " did not survive.";
+        } else if (person.get("Survived").equals("1")) {
+            return " managed to survive.";
+        }
+        return " may or may not have survived";
+    }
+
+    @FXML
+    private void clickUpdateList(ActionEvent event) {
+        this.passengerList.getItems().clear();
+        this.passengerList.setItems(db.readPassengers());
     }
     
 }
--- src/sqlitejavafxapp/data/DatabaseLayer.java
+++ src/sqlitejavafxapp/data/DatabaseLayer.java
@@ -5,8 +5,11 @@
  */
 package sqlitejavafxapp.data;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -23,8 +26,12 @@ import javafx.collections.ObservableList;
  */
 public class DatabaseLayer {
 
-    Connection connection = null;
-
+    /**
+     * Anvender en SELECT-forespørgsel til at hente data fra databasen. For at
+     * modificere dataen der bliver hentet SQL forespørgslen blot ændres.
+     *
+     * @return Finder en liste af alle passagerer.
+     */
     public ObservableList readPassengers() {
         this.connect();
         ObservableList list = FXCollections.observableArrayList();
@@ -40,7 +47,6 @@ public class DatabaseLayer {
                 }
                 list.add(row);
             }
-
         } catch (SQLException ex) {
             Logger.getLogger(DatabaseLayer.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -49,14 +55,63 @@ public class DatabaseLayer {
         return list;
     }
 
+    /**
+     * Anvender en INSERT-forespørgsel til at indsætte en ny passager i
+     * databasen
+     */
+    public void addPassenger(String name, String ticket, String fare) {
+        this.connect();
+        String query = "INSERT INTO titanic_passengers (Name, Ticket, Fare, Age, Sex, Survived) "
+                + "VALUES (?,?,?,?, ?, ?)";
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, name);
+            ps.setString(2, ticket);
+            ps.setString(3, fare);
+            ps.setString(4, "32");
+            ps.setString(5, "Male");
+            ps.setString(6, "0");
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseLayer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.disconnect();
+    }
+
+    /**
+     * Anvender en UPDATE-forespørgsel til at opdatere data for en bestemt
+     * passager
+     */
+    public void updatePassenger() {
+
+    }
+
+    /**
+     * Anvender en DELETE-forespørgsel til at slette en bestemt passager.
+     */
+    public void deletePassenger(HashMap passenger) {
+
+    }
+
+    Connection connection = null;
+
+    /**
+     * Denne funktion bruges til at skabe forbindelse til databasen, og bør ikke
+     * ændres.
+     */
     public void connect() {
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\alex\\Desktop\\Div informatik\\SQLiteJavaFXApp\\src\\sqlitejavafxapp\\data\\data.sqlite");
+            connection = DriverManager.getConnection("jdbc:sqlite:"+Paths.get("").toAbsolutePath().toString()+
+                    "\\src\\sqlitejavafxapp\\data\\titanic_database.sqlite");
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
     }
 
+    /**
+     * Denne funktion bruges til at forbryde forbindelsen til en database, og
+     * bør ikke ændres.
+     */
     public void disconnect() {
         try {
             if (connection != null) {
